bd1d8ae7e1bb46a08b4740e85f5a6876cd50bc0d
PaRom31 <parom.romashoff@yandex.ru>
2022-03-19 16:30:29 +0300

    lesson1

--- src/main/java/ru/gb/lesson1/HomeWorkApp.java
+++ src/main/java/ru/gb/lesson1/HomeWorkApp.java
@@ -16,19 +16,19 @@ public class HomeWorkApp {
         int b = 3;
         int c = a + b;
         if (c >= 0){
-            System.out.println("Сумма положительная");
+            System.out.println("Сумма положительная.");
         }else {
-            System.out.println("Сумма отрицательная");
+            System.out.println("Сумма отрицательная.");
         }
     }
     private static void printColor(){
         int value = 200;
         if (value <= 0){
-            System.out.println("Красный");
+            System.out.println("Красный.");
         }else if ((value > 0) && (value <= 100)) {
-            System.out.println("Желтый");
+            System.out.println("Желтый.");
         }else {
-            System.out.println("Зеленый");
+            System.out.println("Зеленый.");
         }
     }
     private static void compareNumbers(){
