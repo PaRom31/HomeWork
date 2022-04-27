2da77499d58f349da1ee79c5231d9006c89f3f43
PaRom31 <parom.romashoff@yandex.ru>
2022-04-27 22:11:48 +0300

    lesson2.21

---
+++ src/main/java/Level_2/Lesson2/MyArraySizeException.java
@@ -0,0 +1,10 @@
+package Level_2.Lesson2;
+
+public class MyArraySizeException extends ArrayIndexOutOfBoundsException {
+
+    public MyArraySizeException(String massage) {
+        super(massage);
+    }
+
+
+}
